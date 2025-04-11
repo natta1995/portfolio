0266ccde9fe2d1cffc048baa4ab4041546540a32
Natalie <natalie.hallerdal@yahoo.se>
2025-04-11 18:04:32 +0200

    My img added + css

--- src/components/About.js
+++ src/components/About.js
@@ -1,39 +1,48 @@
-import "./Global.css"
+import "./Global.css";
+import profileImg from "../Img/facebook profilbild.jpg";
 
 const About = () => {
   return (
     <div className="component">
       <div className="textdiv">
-      <h3> Kort om mig: </h3>
-      <p>
-        Hej! Jag heter Natalie och jag vill verkligen jobba med kod. Jag är
-        utbildad frontendutvecklare med inriktning på webbsäkerhet, och jag
-        tycker om att bygga lösningar som är tydliga, trygga och användbara.
-      </p>
-      <p>
-        Jag är snäll, driven och en sån som lyssnar, lär och gärna hjälper till.
-        Jag tror att teknik blir som bäst när den kombineras med omtanke – både
-        för användaren och teamet. I mina projekt har jag fokuserat på struktur,
-        responsiv design och funktionallitet. 
-    </p>
-        <p>Jag är nyfiken, lär mig snabbt och
-        trivs i ett arbetsklimat där man får vara både sig själv och utvecklas
-        tillsammans.
-        </p>
-     
-      <p>
-        Är du nyfiken på mig, mina projekt eller bara vill prata frontend? Hör
-        gärna av dig – jag är redo.
-      </p>
+        <h4> Kort om mig: </h4>
 
-      <p>Tre korta om mig:</p>
-      <ul>
-        <li>
-          Älskar att gå i skogen och plocka svamp - till mina näras förtjusning.
-        </li>
-        <li>Älskar böcker, särskillt klassiker som Agatha Christie.</li>
-        <li> Har en stark kärlek till Ada Lovelace.</li>
-      </ul>
+        <p>
+          Hej! Jag heter Natalie och jag vill verkligen jobba med kod. Jag är
+          utbildad frontendutvecklare med inriktning på webbsäkerhet, och jag
+          tycker om att bygga lösningar som är tydliga, trygga och användbara.
+        </p>
+        <p>
+          Jag är snäll, driven och en sån som lyssnar, lär och gärna hjälper
+          till. Jag tror att teknik blir som bäst när den kombineras med omtanke
+          – både för användaren och teamet. I mina projekt har jag fokuserat på
+          struktur, responsiv design och funktionallitet.
+        </p>
+        <p>
+          Jag är nyfiken, lär mig snabbt och trivs i ett arbetsklimat där man
+          får vara både sig själv och utvecklas tillsammans.
+        </p>
+
+        <p>
+          Är du nyfiken på mig, mina projekt eller bara vill prata
+          programmering? Hör gärna av dig – jag är redo.
+        </p>
+        <div className="about-wrapper">
+          <div className="about-text">
+            <h4>Tre korta om mig:</h4>
+            <ul>
+              <li>
+                Älskar att gå i skogen och plocka svamp - till mina näras
+                förtjusning.
+              </li>
+              <li>Älskar böcker, särskillt klassiker som Agatha Christie.</li>
+              <li> Har en stark kärlek till Ada Lovelace.</li>
+            </ul>
+          </div>
+          <div className="about-img">
+            <img src={profileImg} alt="Natalie" className="profile-pic" />
+          </div>
+        </div>
       </div>
     </div>
   );

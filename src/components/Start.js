13267b81b85d86b22606caf7febf53c21886447c
Natalie <natalie.hallerdal@yahoo.se>
2025-04-11 13:30:52 +0200

    Img added

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,10 +1,11 @@
+import "./Start.css";
+
 const Start = () => {
+  return (
+    <div className="start-section">
+      <h1>Hej och välkommen till min portfolio!</h1>
+    </div>
+  );
+};
 
-    return (
-        <div style={{width: "100vw", paddingTop: "150px", paddingBottom: "150px", backgroundColor: "green", marginBottom: "100px"}}>
-            <h1> Hej och välkommen till min porfolio! </h1>
-        </div>
-    )
-}
-
-export default Start; 
+export default Start;

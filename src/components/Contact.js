b8ec0024364ddaa4fae8c543885bc18397400ff4
Natalie <natalie.hallerdal@yahoo.se>
2025-04-11 10:43:34 +0200

    Links to my profiles + small css change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,25 @@ const Contact = () => {
   return (
     <div className="component">
       <div className="textdiv">
-        <h4>Mina kontaktuppgifter</h4>
+        <h4>Mina kontaktuppgifter:</h4>
         <p>OBS: lättast når du mig på email eller via sms</p>
-        <p>E-mail: natalie.hallerdal@yahoo.se</p>
-        <p>Telefonnummer: 0735936556</p>
+        <p><strong>E-mail:</strong> natalie.hallerdal@yahoo.se</p>
+        <p><strong>Telefonnummer:</strong> 0735936556</p>
+        <a
+          href="https://www.linkedin.com/in/natalie-h%C3%A4llerdal-4b2a9499/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        <br></br>
+        <a
+          href="https://github.com/natta1995"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
       </div>
     </div>
   );

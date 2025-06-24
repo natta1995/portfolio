a947d5ae01349c162875833f7eebea57b748d2df
Natalie <natalie.hallerdal@yahoo.se>
2025-06-24 14:24:29 +0200

    Contacts update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,26 +4,38 @@ const Contact = () => {
   return (
     <div className="component">
       <div className="textdiv">
-        <h4>Kontakta mig:</h4>
-        <p><strong>OBS</strong> lättast når du mig på email eller sms</p>
+        <h4>Kontaktuppgifter:</h4>
         <p><strong>E-mail:</strong> natalie.hallerdal@yahoo.se</p>
         <p><strong>Telefonnummer:</strong> +46735936556</p>
+        <p style={{fontSize: "17px"}}>(<strong>OBS</strong> lättast når du mig på email eller sms)</p>
         <br></br>
-        <a
+
+        <table>
+          <tbody>
+            <tr>
+              <td>Besök min linkedIn: </td>
+                  <a
           href="https://www.linkedin.com/in/natalie-h%C3%A4llerdal-4b2a9499/"
           target="_blank"
           rel="noreferrer"
         >
-         Besök min LinkedIn
+         https://www.linkedin.com/in/natalie-h%C3%A4llerdal-4b2a9499/
         </a>
-        <br></br>
+            </tr>
+    
+         <tr>
+        <td>Besök min Github</td>
+       
         <a
           href="https://github.com/natta1995"
           target="_blank"
           rel="noreferrer"
         >
-          Besök min GitHub
+          https://github.com/natta1995
         </a>
+        </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
